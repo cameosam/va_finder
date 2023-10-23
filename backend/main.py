5ce6d111f6be357f75bb3cf5d8bf68abed855092
cameo <cameosam@gmail.com>
2023-10-22 22:08:01 -0700

    add search api

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import search_anime
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/anime/{search_term}")
+async def main(search_term: str, q: str | None = None):
+    return search_anime.search_anime(search_term)
