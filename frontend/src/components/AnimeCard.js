7c8d593c8da3273979e0b91164ad78a93a255f34
cameo <cameosam@gmail.com>
2023-10-28 21:10:26 -0700

    add formatting to cards

--- frontend/src/components/AnimeCard.js
+++ frontend/src/components/AnimeCard.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { Paper, Grid } from "@mui/material";
+import { Paper, Grid, Typography } from "@mui/material";
+import "./AnimeCard.css";
 
 const AnimeCard = (props) => {
-  const title = props.anime.title;
+  const title =
+    props.anime.title.length > 20
+      ? `${props.anime.title.substring(0, 20)}...`
+      : props.anime.title;
   const imageURL = props.anime.images.jpg.image_url;
   return (
-    <Grid item>
-      <Grid container item xs={12}>
-        <Paper>
+    <Grid item className="animeCard__container">
+      <Grid>
+        <Paper className="animeCard__paper">
           <img src={imageURL} alt={title} style={{ maxHeight: 300 }} />
+          <Typography variant="h6" component="h2">
+            {title}
+          </Typography>
         </Paper>
       </Grid>
     </Grid>
--- frontend/src/components/AnimeList.js
+++ frontend/src/components/AnimeList.js
@@ -4,7 +4,7 @@ import { Grid } from "@mui/material";
 
 const AnimeList = (props) => {
   return (
-    <Grid>
+    <Grid container columns={{ xs: 4, sm: 8, md: 12 }}>
       {props.data.map((anime) => (
         <AnimeCard anime={anime} key={anime.mal_id} />
       ))}
