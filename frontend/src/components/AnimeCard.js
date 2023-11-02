431b4aa2853b386d8cdfcdeaedd4fc12fa7e714f
cameo <cameosam@gmail.com>
2023-11-01 23:16:37 -0700

    update card format and spacing

--- frontend/src/components/AnimeCard.js
+++ frontend/src/components/AnimeCard.js
@@ -4,15 +4,15 @@ import "./AnimeCard.css";
 
 const AnimeCard = (props) => {
   const title =
-    props.anime.title.length > 20
-      ? `${props.anime.title.substring(0, 20)}...`
+    props.anime.title.length > 15
+      ? `${props.anime.title.substring(0, 15)}...`
       : props.anime.title;
   const imageURL = props.anime.images.jpg.image_url;
   return (
     <Grid item className="animeCard__container">
       <Grid>
         <Paper className="animeCard__paper">
-          <img src={imageURL} alt={title} style={{ maxHeight: 300 }} />
+          <img src={imageURL} alt={title} style={{ height: 300, width: 200 }} />
           <Typography variant="h6" component="h2">
             {title}
           </Typography>
