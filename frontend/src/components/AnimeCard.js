56895685e44d60bfbd0e201db5e42a39b3a21331
cameo <cameosam@gmail.com>
2023-11-05 15:02:37 -0800

    add full title

--- frontend/src/components/AnimeCard.js
+++ frontend/src/components/AnimeCard.js
@@ -1,44 +1,66 @@
-import React from "react";
-import { Paper, Grid, Typography, Tooltip } from "@mui/material";
+import React, { useState } from "react";
+import { Paper, Grid, Typography, Button } from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const AnimeCard = (props) => {
   const title = props.anime.title;
-  const short_title =
-    title.length > 15 ? `${title.substring(0, 15)}...` : title;
   const imageURL = props.anime.images.jpg.image_url;
+
+  const [show, setShow] = useState(false);
+  const showOverlay = () => {
+    setShow(true);
+  };
+
+  const hideOverlay = () => {
+    setShow(false);
+  };
   return (
     <Grid item sx={{ padding: "5px" }}>
-      <Tooltip title={title}>
-        <Grid>
-          <Paper
-            sx={[
-              {
-                display: "flex",
-                alignItems: "center",
-                alignContent: "center",
-                justifyContent: "center",
-                flexDirection: "column",
-                padding: "10px",
-              },
-              {
-                "&:hover": {
-                  backgroundColor: "lightgray",
-                  opacity: "0.8",
+      <Grid>
+        <Paper
+          onMouseOver={showOverlay}
+          onMouseLeave={hideOverlay}
+          sx={[
+            {
+              position: "relative",
+              display: "inline-flex",
+              alignItems: "center",
+              alignContent: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              padding: "10px",
+            },
+          ]}
+        >
+          <img src={imageURL} alt={title} style={{ height: 300, width: 200 }} />
+
+          {show && (
+            <Typography
+              variant="h6"
+              sx={[
+                {
+                  display: "inline-flex",
+                  position: "absolute",
+                  top: "0",
+                  padding: "10px",
+                  width: "85%",
+                  backgroundColor: "rgba(255, 255, 255, 0.5)",
                 },
-              },
-            ]}
-          >
-            <img
-              src={imageURL}
-              alt={title}
-              style={{ height: 300, width: 200 }}
-            />
-            <Typography variant="h6" component="h2">
-              {short_title}
+              ]}
+            >
+              {title}
             </Typography>
-          </Paper>
-        </Grid>
-      </Tooltip>
+          )}
+          <Button
+            size="small"
+            variant="outlined"
+            endIcon={<ArrowForwardIcon />}
+            sx={{ alignSelf: "stretch", margin: "5px 0 0 0" }}
+          >
+            Select
+          </Button>
+        </Paper>
+      </Grid>
     </Grid>
   );
 };
