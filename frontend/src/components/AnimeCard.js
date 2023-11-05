a091877149f3c3361f2ef8c899e1893c72d951b2
cameo <cameosam@gmail.com>
2023-11-05 10:28:17 -0800

    have hover text to display full anime title

--- frontend/src/components/AnimeCard.js
+++ frontend/src/components/AnimeCard.js
@@ -1,23 +1,28 @@
 import React from "react";
-import { Paper, Grid, Typography } from "@mui/material";
+import { Paper, Grid, Typography, Tooltip } from "@mui/material";
 import "./AnimeCard.css";
 
 const AnimeCard = (props) => {
-  const title =
-    props.anime.title.length > 15
-      ? `${props.anime.title.substring(0, 15)}...`
-      : props.anime.title;
+  const title = props.anime.title;
+  const short_title =
+    title.length > 15 ? `${title.substring(0, 15)}...` : title;
   const imageURL = props.anime.images.jpg.image_url;
   return (
     <Grid item className="animeCard__container">
-      <Grid>
-        <Paper className="animeCard__paper">
-          <img src={imageURL} alt={title} style={{ height: 300, width: 200 }} />
-          <Typography variant="h6" component="h2">
-            {title}
-          </Typography>
-        </Paper>
-      </Grid>
+      <Tooltip title={title}>
+        <Grid>
+          <Paper className="animeCard__paper">
+            <img
+              src={imageURL}
+              alt={title}
+              style={{ height: 300, width: 200 }}
+            />
+            <Typography variant="h6" component="h2">
+              {short_title}
+            </Typography>
+          </Paper>
+        </Grid>
+      </Tooltip>
     </Grid>
   );
 };
