8fb66b35c81d2d6fe565d8f2861ca297233df18f
cameo <cameosam@gmail.com>
2023-11-05 16:38:03 -0800

    add more api calls

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import search_anime
+import get_characters
+import va_credits
 
 app = FastAPI()
 
@@ -16,5 +18,15 @@ app.add_middleware(
 
 
 @app.get("/anime/{search_term}")
-async def main(search_term: str):
+async def get_anime(search_term: str):
     return search_anime.search_anime(search_term)
+
+
+@app.get("/characters/{anime_id}")
+async def get_chars(anime_id: str):
+    return get_characters.get_characters(anime_id)
+
+
+@app.get("/va_roles/{va_id}")
+async def get_va_roles(va_id: str):
+    return va_credits.va_credits(va_id)
